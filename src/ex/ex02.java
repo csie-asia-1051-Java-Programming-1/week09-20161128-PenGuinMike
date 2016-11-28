95936f8bcb011c12318770675303c14ed3c90389
PenGuinMike <104021074@live.asia.edu.tw>
2016-11-28 16:20:34 +0800

    save

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -7,11 +7,44 @@
  * Author: 1050210XX 周永振老師
  */
 
+import java.util.ArrayList;
+
+import ex.Student;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Student>student=new ArrayList<Student>();
+		student.add(new Student("Mike","1024"));
+		student.get(0).showInfo();
 	}
+}
 
+
+class Student{
+		private String id,name;
+		//private ArrayList
+		public Student (String name1,String id1){
+			name=name1;id=id1;
+		}
+		public void addCourse(String name1,String id1){
+			
+		}
+		public void showInfo() {
+			System.out.println("name: "+name);
+			System.out.println("id: "+id);
+			//System.out.println("#of course:"+couList.size());
+		}
+	}
+class Course{
+	private String couName,couId,teaCher;
+	private int score;
+	public Course(String name1,String id1){
+		couName=name1;couId=id1;
+	}
+	public String getName(){
+		return couName;
+	}
+	
+	
 }
