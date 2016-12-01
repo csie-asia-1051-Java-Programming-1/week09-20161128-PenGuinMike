6019b646e154f6f9b15599f8b01307959f377640
PenGuinMike <104021074@live.asia.edu.tw>
2016-12-01 17:54:20 +0800

    數列

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -4,13 +4,25 @@ package hw;
 數列一:1 2 4 7 11 16 22 29…
 數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 104021074 楊永聖
  */
+import java.util.*;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt(),sum=1,i=0;
+		while(sum<x){
+			sum+=i;
+			i++;
+			System.out.print(sum+" ");
+		}System.out.println();
+		for(int a=1;a<=x+1;a++){
+			for(int b=0;b<a;b++){
+				System.out.print(a);
+			}
+		}
 	}
 
 }
